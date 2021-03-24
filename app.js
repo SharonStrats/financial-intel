18d696739dc1ee82ae4cb561d91f229d5c586656
Sharon <sstratsianis@student.unimelb.edu.au>
2021-03-24 23:22:11 +1100

    needed to change the port

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use('/', index);
 
 module.exports = app;
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function () {
   console.log('Listening on ' + port);
 });
